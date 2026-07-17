6f5ebddf6c57e2e714afcb772f910f3160ff7145
jaiganesh2108 <jaig7335@gmail.com>
2026-07-17 20:55:03 +0530

    feat: null to the models

--- myproject/members/models.py
+++ myproject/members/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone = models.IntegerField()
-    joined_date = models.DateField()
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
