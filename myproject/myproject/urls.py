3e8665744db4ac0b4d99e872b06dfde04c7efe0f
jaiganesh2108 <jaig7335@gmail.com>
2026-07-18 12:16:21 +0530

    feat: feature implementations display

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from members import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('members/', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
