e6bf0f9269c2421f283d69f2de4422d665451f2c
jaiganesh2108 <jaig7335@gmail.com>
2026-07-19 11:36:25 +0530

    feat: testing

--- myproject/members/urls.py
+++ myproject/members/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.members, name='members'),
     path('details/<int:id>/', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('testing/', views.testing, name='testing'),
     path('members/', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
